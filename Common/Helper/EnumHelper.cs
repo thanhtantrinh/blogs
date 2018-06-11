3a786bc462ae22a559190313b63faa893e6e7a54
thanhtantrinh <thanhtan.hello@gmail.com>
2018-06-12 01:00:14 +0700

    new

---
+++ Common/Helper/EnumHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Reflection;
+
+    public static class EnumExtensions
+    {
+        /// <summary>
+        ///     A generic extension method that aids in reflecting 
+        ///     and retrieving any attribute that is applied to an 'Enum`.
+        /// </summary>
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue) where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<TAttribute>();
+        }
+
+        public static string GetDisplayName(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+
+            if (field == null)
+                return String.Empty;
+
+            object[] attribs = field.GetCustomAttributes(typeof(DisplayAttribute), true);
+            if (attribs.Length > 0)
+            {
+                return ((DisplayAttribute)attribs[0]).GetName();
+            }
+            return value.ToString();
+        }
+
+    }
+}
